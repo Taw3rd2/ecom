f59083f36915ebcb430663e55ba8e813855531f2
Thomas Waldorf <Zeeroh71@gmail.com>
2020-04-01 15:33:33 -0400

    update out menu-items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -7,35 +7,40 @@ const Directory = () => {
     {
       title: "hats",
       imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-      id: 1
+      id: 1,
+      linkUrl: "hats"
     },
     {
       title: "jackets",
       imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-      id: 2
+      id: 2,
+      linkUrl: ""
     },
     {
       title: "sneakers",
       imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-      id: 3
+      id: 3,
+      linkUrl: ""
     },
     {
       title: "womans",
       imageUrl: "https://i.ibb.co/GCCdy8t/womans.png",
       id: 4,
+      linkUrl: "",
       size: "large"
     },
     {
       title: "mens",
       imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
       id: 5,
+      linkUrl: "",
       size: "large"
     }
   ];
   return (
     <div className="directory-menu">
-      {sections.map(({ title, imageUrl, id, size }) => (
-        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
       ))}
     </div>
   );
